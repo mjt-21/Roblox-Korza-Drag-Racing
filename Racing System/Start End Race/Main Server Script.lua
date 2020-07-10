6e5936beb6e6e2af4c31ff41848094c75da47ebe
Matthew T <65698531+MJTFreeTime@users.noreply.github.com>
2020-07-09 21:01:27 -0700

    Includes RaceTime Remote Event FireAllClients()

--- Racing System/Start End Race/Main Server Script.lua
+++ Racing System/Start End Race/Main Server Script.lua
@@ -3,6 +3,7 @@ local CountdownTime = ReplicatedStorage:WaitForChild("TESTVALUE")
 local LapTime = ReplicatedStorage:WaitForChild("LapTime")
 local RacingBool = ReplicatedStorage:WaitForChild("RacingBool")
 local FinishLine = game.Workspace["Racing System"].FinishLine
+local RaceTimeRE = ReplicatedStorage:WaitForChild("RaceTimeRE")
 
 FinishLine.Touched:Connect(function()
 	RacingBool.Value = true
@@ -26,6 +27,7 @@ end
 
 CountdownTime.Changed:Connect(function()
 	if CountdownTime.Value == 0 then
+		RaceTimeRE:FireAllClients()
 		StartEnd()
    end
 end)
