fb1a5ef4b7c94127601775a80452bb267e3f43c0
Matthew T <65698531+MJTFreeTime@users.noreply.github.com>
2020-08-04 00:29:04 -0700

    Now works with Countdown

---
+++ Racing System/Test System/RacingSystem2.lua
@@ -0,0 +1,13 @@
+-- Variables
+local RacingBool2 = game.ServerStorage.RacingBools.RacingBool2
+local RaceTime2 = game.ServerStorage.PlayerTimes.Time2
+
+RacingBool2.Changed:Connect(function()
+	if RacingBool2.Value == true then
+		local startTime = tick()
+		while RacingBool2.Value == true do
+			game:GetService("RunService").Heartbeat:Wait()
+			RaceTime2.Value = tick() - startTime
+		end 
+	end
+end)
