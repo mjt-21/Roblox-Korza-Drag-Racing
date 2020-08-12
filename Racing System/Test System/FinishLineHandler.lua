1e9d06fe52e37afdd4417dbab1a383728d47d327
Matthew T <65698531+MJTFreeTime@users.noreply.github.com>
2020-08-11 22:54:49 -0700

    Update FinishLineHandler.lua

--- Racing System/Test System/FinishLineHandler.lua
+++ Racing System/Test System/FinishLineHandler.lua
@@ -2,11 +2,12 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 -- Variables
-local FinishLine = game.Workspace.FinishLine
-local BoolValue = game.ServerStorage.FinishLineActivate
+local FinishLine = game.Workspace["Racing System"].FinishLine
+local BoolValue = game.ServerStorage.RacingSystemStorage.FinishLineActivate
 local ReceiverBindable = ReplicatedStorage:WaitForChild("FinishLine")
 local BindableRaceEndRemote = ReplicatedStorage:WaitForChild("BindableGUIRaceEnd")
 local RaceEndRemote = ReplicatedStorage:WaitForChild("GUIRaceEnd")
+local raceInProgress = game.ServerStorage.RacingSystemStorage.RaceInProgress
 
 local playersBlackList = {} -- Table to temporarily store players' that touched FinishLine already
 
@@ -21,22 +22,31 @@ function foundInList(player)
 end
 
 FinishLine.Touched:Connect(function(hit) -- "hit" is the part that hits the FinishLine
+	if raceInProgress.Value == true then
+		local PlayerThatTouched = nil
+		PlayerName = nil
 	
-	-- If part that touches player is a Player, it makes a PlayerName variable
-	local PlayerThatTouched = game.Players:GetPlayerFromCharacter(hit.Parent)
-	if game.Players:GetPlayerFromCharacter(hit.Parent)then
-		PlayerName = tostring(hit.Parent.Name)
-	end
+		-- Sees if the part that's hit has a VehicleSeat, then gets the player that is occupying it, and puts the player object and name in two variables
+		local children = hit.Parent.Parent:GetChildren()
+		for _, child in pairs(children) do
+			if child:IsA("VehicleSeat") then
+				local player = child.Occupant.Parent
+				PlayerThatTouched = game.Players:GetPlayerFromCharacter(player)
+				PlayerName = child.Occupant.Parent.Name
+			end
+		end	
 	
-	if PlayerName then
-		if not foundInList(PlayerName) then -- Checks if PlayerName is not in the blacklist table
-			table.insert(playersBlackList, PlayerName) -- Inserts PlayerName into blacklist table
+		-- The below makes it so that a player cannot hit the FinishLine and fire off the code more than once in a single match/race
+		if PlayerName then
+			if not foundInList(PlayerName) then -- Checks if PlayerName is not in the blacklist table
+				table.insert(playersBlackList, PlayerName) -- Inserts PlayerName into blacklist table
 			
-			if game.Players:GetPlayerFromCharacter(hit.Parent)then
-				BindableRaceEndRemote:Fire(PlayerThatTouched, PlayerName)
-				BoolValue.Value = true
-				if BoolValue.Value == true then
-					BoolValue.Value = false
+				if PlayerThatTouched then
+					BindableRaceEndRemote:Fire(PlayerThatTouched, PlayerName)
+					BoolValue.Value = true
+					if BoolValue.Value == true then
+						BoolValue.Value = false
+					end
 				end
 			end
 		end
